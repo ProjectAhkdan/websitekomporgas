0ae19bb7772971976f98e7286eaabdb4f5edca77
Ahkdan Prika Nurwicaksono <ahkdanprika1@gmail.com>
2025-01-12 01:16:51 +0700

    Update heroHome.tsx

--- src/app/Home/heroHome.tsx
+++ src/app/Home/heroHome.tsx
@@ -17,7 +17,7 @@ const HeroHome = () => {
       }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content py-10 text-neutral-content text-center">
+      <div className="hero-content py-20 text-neutral-content text-center">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">
             Jasa Service Kompor Gas Tanam & Oven No.1
