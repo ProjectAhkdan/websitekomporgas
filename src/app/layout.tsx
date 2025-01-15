1e787747de088f198a7a4d784f87b24cc114e6ea
ProjectAhkdan <ahkdanprika1@gmail.com>
2025-01-15 15:07:12 +0700

    update ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google"; 
+import { Poppins } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import CallatAction from "@/components/callatAction";
 import WhatsAppChat from "@/components/floatingWa";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const poppins = Poppins({
-  weight: ["400", "500", "600", "700"], 
+  weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
   variable: "--font-poppins",
 });
@@ -26,9 +28,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} antialiased relative`}
-      >
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5T3SQC77');`}
+        </Script>
+      </head>
+      <body className={`${poppins.variable} antialiased relative`}>
+        <SpeedInsights />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5T3SQC77"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Navbar />
         {children}
         <WhatsAppChat />
